d2695d128e05e8395347da4485b116bd9a8ee7c3
ericm48 <me_manley@yahoo.com>
2022-12-22 11:09:23 -0700

    -Updated scripts, removed old.

--- sh/setup-env-variables-basic.sh
+++
@@ -1,5 +0,0 @@
-# 
-
-export RESOURCE_GROUP='azure-asa-uswest'       														# existing resource group or one that will be created in next steps
-export SPRING_APPS_SERVICE='hello-spring-controller-basic' 									# name of the service that will be created in the next steps
-export REGION='westus'	 												                         	# choose a region with Enterprise tier support
